cb382795799d9e12fd03f0a9e7e116c0c89fba61
Acer <Acer@DESKTOP-K09QF87>
2022-04-16 19:11:24 +0200

    Added classes/robotsList.php and interfaces/robotsListError.php

---
+++ classes/robotsList.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ViewsCounter\Classes;
+
+use ViewsCounter\Interfaces\RobotsListError;
+
+//This class is useful to fetch the robots list from a URL and parse to an array
+class RobotsList implements RobotsListError{
+
+}
+
+?>
---
+++ interfaces/robotsListError.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ViewsCounter\Interfaces;
+
+//This interface contains error code and error messages of RobotsList class
+interface RobotsListError{
+
+}
+?>
